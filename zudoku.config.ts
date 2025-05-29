777ae41e568671c40da548856499a41cc811f32a
ivanochkoivan <59032932+ivanochkoivan@users.noreply.github.com>
2025-05-29 20:28:38 +0300

    Update zudoku.config.ts

--- zudoku.config.ts
+++ zudoku.config.ts
@@ -1,6 +1,7 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
+  baseUrl: "/ecommerce-api-docs-myself",    // <-- додано baseUrl
   topNavigation: [
     { id: "docs", label: "Documentation" },
     { id: "api", label: "API Reference" },
@@ -25,8 +26,4 @@ const config: ZudokuConfig = {
   },
 };
 
-export default {
-  outDir: 'dist',
-  root: './pages',
-  base: '/ecommerce-api-docs-myself/',  // Додаємо ось цей параметр
-}
+export default config;
